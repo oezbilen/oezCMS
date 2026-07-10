fc384ff6c4e67e07db8833d8afa03371956d81bc
Mehmet Özbilen <develop@oezbilen.net>
2026-07-10 19:42:56 +0000

    feat(container): add lazy singleton set and get
    Add a Container that registers factories via set() and resolves them
    lazily via get(), caching each result so repeated calls return the same
    instance. Generic over the service type for type-safe resolution. Makes
    the set/get tests pass (GREEN phase).
    
    Refs: feature/container

---
+++ src/Core/Container.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OezCMS\Core;
+
+final class Container
+{
+    /**
+     * @var array<class-string, callable(self): object>
+     */
+    private array $factories = [];
+
+    /**
+     * @var array<class-string, object>
+     */
+    private array $instances = [];
+
+    /**
+     * @template T of object
+     *
+     * @param class-string<T>   $id
+     * @param callable(self): T $factory
+     */
+    public function set(string $id, callable $factory): void
+    {
+        $this->factories[$id] = $factory;
+
+        unset($this->instances[$id]);
+    }
+
+    /**
+     * @template T of object
+     *
+     * @param class-string<T> $id
+     *
+     * @return T
+     */
+    public function get(string $id): object
+    {
+        if (isset($this->instances[$id])) {
+            /** @var T $instance */
+            $instance = $this->instances[$id];
+
+            return $instance;
+        }
+
+        $service = ($this->factories[$id])($this);
+        $this->instances[$id] = $service;
+
+        /** @var T $service */
+        return $service;
+    }
+}
